9bc9bb57517302487b6e5ebf6b57df157d961101
Yana <yanazlo0219@gmail.com>
2021-03-17 21:21:21 +0500

    Import geometry.cpp config

--- geometry.cpp
+++ geometry.cpp
@@ -1,28 +1,69 @@
-#include <iostream>
-
-using namespace std;
+#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cmath>
 
 int main()
 {
-	setlocale(LC_ALL,"Russian");
-    string c;
-    float xc, yc; //координаты для окружности
-  //  float x1, x2, x3; // абсциссы для треугольника
-  //  float y1, y2, y3; // ординаты треугольника
-    float pi=3.1415;
-    float R; // радиус окружности
-    float perimeter, area;
+char input[1000]="";
+int k = 0;
+char* buffer;
+bool isRight = true;
+while (strcmp(input, "exit"))
+{
+scanf("%[^\n]s", input);
 
- 
-  cin >> c;
-   if(c=="circle") {
-    cin >> xc >> yc >> R;
-    perimeter = 2*pi*R;
-    area = pi*R*R;
-    cout << "perimeter = "<< perimeter<< '\n';
-    cout << "area = "<< area;
+buffer = strtok(input," .,()");
+if (_strcmpi(buffer, "circle") == 0)
+{
+float x, y, R, perimeter, area; 
+buffer = strtok(NULL, " ,()");
+x = atof(buffer);
+buffer = strtok(NULL, " ,()");
+y = atof(buffer);
+buffer = strtok(NULL, " ,()");
+R = atof(buffer);
+if (R <= 0)
+{
+printf("Error. Radius must be positiv.\n");
+getchar();
+continue;
 }
-
-
-    return 0;
+perimeter = 2 * R * 3.1415926536;
+area = R * R * 3.1415926536;
+printf("perimeter=%f\narea=%f", perimeter, area);
+}
+else if (_strcmpi(buffer, "triangle") == 0)
+{
+float x[4], y[4];
+for (int i = 0; i < 4; i++)
+{
+buffer = strtok(NULL, " ,()");
+x[i] = atof(buffer);
+buffer = strtok(NULL, " ,()");
+y[i] = atof(buffer);
+}
+float perimeter, area,a,b,c;
+a = sqrt((x[1] - x[0]) * (x[1] - x[0]) + (y[1] - y[0]) * (y[1] - y[0]));
+b = sqrt((x[2] - x[0]) * (x[2] - x[0]) + (y[2] - y[0]) * (y[2] - y[0]));
+c = sqrt((x[2] - x[1]) * (x[2] - x[1]) + (y[2] - y[1]) * (y[2] - y[1]));
+perimeter = a + b + c;
+area = sqrt(perimeter/2*(perimeter/2-a)*(perimeter/2-c)*(perimeter/2 - b));
+printf("perimeter=%f\narea=%f\n",perimeter,area);
+}
+/*else if (_strcmpi(buffer, "polygon") == 0)
+{
+;
+}*/
+else
+{
+printf("Error. Expected \"triangle\" or \"circle\".");
+getchar();
+continue;
+}
+getchar();
+}
+getchar();
+return 0;
 }
