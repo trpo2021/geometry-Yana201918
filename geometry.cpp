8a501cef834638188bf2bcf889c3478ebbd42750
Yana <yanazlo0219@gmail.com>
2021-03-18 12:17:50 +0500

    Import geometry.cpp config

--- geometry.cpp
+++ geometry.cpp
@@ -9,7 +9,7 @@ int main()
 char input[1000]="";
 int k = 0;
 char* buffer;
-bool isRight = true;
+
 while (strcmp(input, "exit"))
 {
 scanf("%[^\n]s", input);
@@ -17,7 +17,7 @@ scanf("%[^\n]s", input);
 buffer = strtok(input," .,()");
 if (_strcmpi(buffer, "circle") == 0)
 {
-float x, y, R, P, S; 
+float x, y, R, perimeter, area; 
 buffer = strtok(NULL, " ,()");
 x = atof(buffer);
 buffer = strtok(NULL, " ,()");
@@ -30,9 +30,9 @@ printf("Error. Radius must be positiv.\n");
 getchar();
 continue;
 }
-P = 2 * R * 3.1415926536;
-S = R * R * 3.1415926536;
-printf("P=%f\nS=%f", P, S);
+perimeter = 2 * R * 3.1415926536;
+area = R * R * 3.1415926536;
+printf("perimeter=%f\narea=%f", perimeter, area);
 }
 else if (_strcmpi(buffer, "triangle") == 0)
 {
@@ -44,13 +44,13 @@ x[i] = atof(buffer);
 buffer = strtok(NULL, " ,()");
 y[i] = atof(buffer);
 }
-float P, S,a,b,c;
+float perimeter, area,a,b,c;
 a = sqrt((x[1] - x[0]) * (x[1] - x[0]) + (y[1] - y[0]) * (y[1] - y[0]));
 b = sqrt((x[2] - x[0]) * (x[2] - x[0]) + (y[2] - y[0]) * (y[2] - y[0]));
 c = sqrt((x[2] - x[1]) * (x[2] - x[1]) + (y[2] - y[1]) * (y[2] - y[1]));
-P = a + b + c;
-S = sqrt(P/2*(P/2-a)*(P/2-c)*(P/2 - b));
-printf("P=%f\nS=%f\n",P,S);
+perimeter = a + b + c;
+area = sqrt(perimeter/2*(perimeter/2-a)*(perimeter/2-c)*(perimeter/2 - b));
+printf("perimeter=%f\narea=%f\n",perimeter,area);
 }
 /*else if (_strcmpi(buffer, "polygon") == 0)
 {
