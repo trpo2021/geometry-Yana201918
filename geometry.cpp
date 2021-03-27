29de0234e91bbe6a5365047fb54c6cb2081ec7bc
Yana <yanazlo0219@gmail.com>
2021-03-27 18:47:13 +0500

    Create structure

--- geometry.cpp
+++ geometry.cpp
@@ -8,14 +8,15 @@ int main()
 char input[1000]="";
 char* buffer;
 
-while (strcmp(input, "exit"))
+//Программа не закончится, пока пользователь не введёт "exit"
 {
 scanf("%[^\n]s", input);
+if (strcmp(input, "exit"))== 1;
 buffer = strtok(input," .,()");
 
-if (_strcmpi(buffer, "circle") == 0)
+if (_strcmpi(buffer, "circle") == 0)// Если пользователь ввёл "circle"
 {
-float x, y, R, P, S; // ????????
+float x, y, R, perimeter, area; 
 buffer = strtok(NULL, " ,()");
 x = atof(buffer);
 buffer = strtok(NULL, " ,()");
@@ -24,15 +25,15 @@ buffer = strtok(NULL, " ,()");
 R = atof(buffer);
 if (R <= 0)
 {
-printf("Error. Radius must be positiv.\n");
+printf("Error. Radius must be positiv.\n\n"); //Проверка радиуса
 getchar();
 continue;
 }
-P = 2 * R * 3.1415926536;
-S = R * R * 3.1415926536;
-printf("P=%f\nS=%f", P, S);
+perimeter = 2 * R * 3.1415926536;
+area = R * R * 3.1415926536;
+printf("perimeter=%f\narea=%f\n\n", perimeter, area);
 }
-else if (_strcmpi(buffer, "triangle") == 0)
+else if (_strcmpi(buffer, "triangle") == 0) //Если пользователь введёл "triangle"
 {
 float x[4], y[4];
 for (int i = 0; i < 4; i++)
@@ -42,20 +43,21 @@ x[i] = atof(buffer);
 buffer = strtok(NULL, " ,()");
 y[i] = atof(buffer);
 }
-float P, S,a,b,c;
+float perimeter, area,a,b,c;
 a = sqrt((x[1] - x[0]) * (x[1] - x[0]) + (y[1] - y[0]) * (y[1] - y[0]));
 b = sqrt((x[2] - x[0]) * (x[2] - x[0]) + (y[2] - y[0]) * (y[2] - y[0]));
 c = sqrt((x[2] - x[1]) * (x[2] - x[1]) + (y[2] - y[1]) * (y[2] - y[1]));
-P = a + b + c;
-S = sqrt(P/2*(P/2-a)*(P/2-c)*(P/2 - b));
-printf("P=%f\nS=%f\n",P,S);
+perimeter = a + b + c;
+float p=perimeter/2;
+area = sqrt(p*(p - a)*(p - c)*(p - b));
+printf("perimeter=%f\narea=%f\n\n",perimeter,area);
 }
 
 else
 {
-printf("Error. Expected \"triangle\" or \"circle\".");
-getchar();
-continue;
+	printf("Error. Expected \"triangle\" or \"circle\".\n\n");
+	getchar();
+	continue;
 }
 getchar();
 }
