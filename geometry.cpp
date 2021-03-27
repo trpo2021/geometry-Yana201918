95ce2f5bbe8c68f25a7ab1db870b1a605449f1e3
Yana <yanazlo0219@gmail.com>
2021-03-27 19:10:59 +0500

    change code

--- geometry.cpp
+++ geometry.cpp
@@ -3,21 +3,42 @@
 #include <stdlib.h>
 #include <cmath>
 
+void Circle()
+{
+    char* buffer;
+    float x, y, R, perimeter, area;
+    buffer = strtok(NULL, " ,()");
+    x = atof(buffer);
+    buffer = strtok(NULL, " ,()");
+    y = atof(buffer);
+    buffer = strtok(NULL, " ,()");
+    R = atof(buffer);
+    if (R <= 0) {
+        printf("Error. Radius must be positiv.\n\n"); //Проверка радиуса
+        getchar();
+    }
+    perimeter = 2 * R * 3.1415926536;
+    area = R * R * 3.1415926536;
+    printf("perimeter=%f\narea=%f\n\n", perimeter, area);
+    delete buffer;
+}
+
 int main()
 {
     char input[1000] = "";
     char* buffer;
+    float x, y, R, perimeter, area;
 
     //Программа не закончится, пока пользователь не введёт "exit"
     {
         scanf("%[^\n]s", input);
-        if (strcmp(input, "exit"))
-            == 1;
+
         buffer = strtok(input, " .,()");
 
         if (_strcmpi(buffer, "circle") == 0) // Если пользователь ввёл "circle"
+
         {
-            float x, y, R, perimeter, area;
+            Circle;
             buffer = strtok(NULL, " ,()");
             x = atof(buffer);
             buffer = strtok(NULL, " ,()");
@@ -27,12 +48,13 @@ int main()
             if (R <= 0) {
                 printf("Error. Radius must be positiv.\n\n"); //Проверка радиуса
                 getchar();
-                continue;
             }
+
             perimeter = 2 * R * 3.1415926536;
             area = R * R * 3.1415926536;
             printf("perimeter=%f\narea=%f\n\n", perimeter, area);
         }
+
         else if (_strcmpi(buffer, "triangle") == 0) //Если пользователь введёл "triangle"
         {
             float x[4], y[4];
@@ -55,7 +77,6 @@ int main()
         else {
             printf("Error. Expected \"triangle\" or \"circle\".\n\n");
             getchar();
-            continue;
         }
         getchar();
     }
